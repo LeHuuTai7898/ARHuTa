f6dc9ef021d998fe86499bf44283f03e74555af5
LeHuuTai7898 <tailh07081998@gmail.com>
2019-10-29 11:11:26 +0700

    ARHuTa

---
+++ ARHuTaWebProject/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ARHuTaWebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<String> images = new List<string>();
+            images.Add("~/Content/Images/slide-1.jpg");
+            ViewData["SlidesSrc"] = images;
+            return View();
+        }
+
+      
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index","Login");
+        }
+
+        public ActionResult CreateAccout()
+        {
+            return RedirectToAction("Index", "CreateAccount");
+
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Men()
+        {
+            return View();
+        }
+
+        public ActionResult Women()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
---
+++ ARHuTaWebProject/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ARHuTaWebProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void onSingInClick()
+        {
+
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public ActionResult CreateAccount()
+        {
+            return RedirectToAction("Index", "CreateAccount");
+        }
+        public ActionResult ReturnToStore()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
